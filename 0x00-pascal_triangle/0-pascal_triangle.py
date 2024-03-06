3ad0164fd48071d6cd4f64919836f4aab0dbd0c1
SolomonOdunusi <solomonodunusi@gmail.com>
2024-03-05 17:40:37 -0800

    Debugged the pascal triangle algorithm

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,7 +8,7 @@ def pascal_triangle(n):
         row = [1]
         for i in range(1, height):
             row.append(triangle[height - 1][i - 1] + triangle[height - 1][i])
-        if triangle != 0:
+        if height > 0:
             row.append(1)
         triangle.append(row)
     return triangle
