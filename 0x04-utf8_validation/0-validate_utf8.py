db0a008bbbdd9f40e9b0e8f9cdf39206c7af9f6a
SolomonOdunusi <solomonodunusi@gmail.com>
2024-04-03 17:43:59 +0000

    Added th utf script

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+UTF Validity Testing
+"""
+
+
+def validUTF8(data):
+    """Utf-8 validity testing"""
+    count = 0
+    for num in data:
+        if count == 0:
+            if num >> 5 == 0b110:
+                count = 1
+            elif num >> 4 == 0b1110:
+                count = 2
+            elif num >> 3 == 0b11110:
+                count = 3
+            elif num >> 7:
+                return False
+        else:
+            if num >> 6 != 0b10:
+                return False
+            count -= 1
+    return count == 0
