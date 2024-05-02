1bd5aeb33ba1fc1533c1c4b15a43b4bee57f1878
SolomonOdunusi <solomonodunusi@gmail.com>
2024-05-02 20:36:21 +0000

    Debugging

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,28 +3,28 @@
 
 
 def makeChange(coins, total):
-    """Given a pile of coins of
-    different values, determine
-    the fewest number of coins
+    """Determine the fewest number of coins
     needed to meet a given amount
     total
     """
-    # If total is 0 or less, return 0
-    if total <= 0:
+    if(total <= 0):
         return 0
-    
-    # Create a list to store the minimum number of coins needed to make each total from 0 to total
-    min_coins = [float('inf')] * (total + 1)
-    min_coins[0] = 0
-    
-    # Iterate through each coin value
-    for coin in coins:
-        # Update the min_coins list for each total from coin value to total
-        for i in range(coin, total + 1):
-            min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
-    
-    # If the min_coins list at the total is still infinity, it means the total cannot be met by any number of coins you have
-    if min_coins[total] == float('inf'):
+
+    placeholder = total + 1
+
+    min_coin = {0: 0}
+
+    for i in range(1, total + 1):
+        min_coin[i] = placeholder
+
+        for coin in coins:
+            current = i - coin
+            if current < 0:
+                continue
+
+            min_coin[i] = min(min_coin[current] + 1, min_coin[i])
+
+    if min_coin[total] == total + 1:
         return -1
-    else:
-        return min_coins[total]
+
+    return min_coin[total]
